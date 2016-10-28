732783104473dc8683e34f890bf1cc5ab0eabdff
Azad94 <sheraz.azad@stud.fh-luebeck.de>
2016-10-28 23:15:12 +0200

    Init() Methode teilweise implementiert

--- src/praktikum_1/DevelopingEnvironment.java
+++ src/praktikum_1/DevelopingEnvironment.java
@@ -3,7 +3,6 @@ package praktikum_1;
 import java.util.LinkedList;
 import java.util.List;
 
-
 /**
  *
  * Created by Sheraz on 27.10.16.
@@ -12,6 +11,16 @@ public class DevelopingEnvironment {
 
     int lockerAmount = getLockerAmount();
     List<Locker> lockers;
+
+    //time the studio opens
+    int openingHours;
+    //time the studio closes
+    int closingTime;
+
+    int currentTime;
+
+    boolean focusPersonArrived;
+
 
     public int getLockerAmount(){
         return 0;
@@ -25,22 +34,33 @@ public class DevelopingEnvironment {
     }
 
     /**
-     * Initializes all Locker Objects and
+     * Initializes all Locker Objects and set their neighbours
      * finds its neighbours
      */
     private void init(){
         lockers = new LinkedList<Locker>();
-        for(int i = 0; i < lockerAmount; i++){
+        Locker dummyLocker;
+        focusPersonArrived = false;
 
+        //TODO MALTE
+        //TODO die zeiten festelegenich hab das noch nivht verstanden was Jonas meinte
+        //TODO ich kann aber natürlich nach fragen wenn du das nicht machen möchtest
+        openingHours = 10;
+        closingTime = 22;
+
+        for(int i = 0; i < lockerAmount; i++){
+            lockers.add(i, dummyLocker = new Locker(i,0, 0, 0, null));
+            dummyLocker.setNeighbours(i, lockerAmount);
         }
 
     }
 
-    /**
-     * Searches the neighbours of one Locker
-     * @return
-     */
-    private List<Integer> neighbours(){
-        return null;
+    public boolean checkForFocusPerson(){
+
+        //TODO die Zeit muss hier angepasst werden auf den richtigen Datentyp
+        if(currentTime > 1445){
+            return focusPersonArrived = true;
+        }
+        else return focusPersonArrived = false;
     }
 }

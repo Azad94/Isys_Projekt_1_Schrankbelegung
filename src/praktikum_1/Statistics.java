af1bfe0095ac3bf55bc6d74b203fe6ee99d4ff27
Azad94 <sheraz.azad@stud.fh-luebeck.de>
2016-10-30 11:09:20 +0100

    ändern der Häufigkeiten nach einem Durchlauf Erstellen der File mit den Belegungszeiten erledigt

--- src/praktikum_1/Statistics.java
+++ src/praktikum_1/Statistics.java
@@ -1,8 +1,19 @@
 package praktikum_1;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Statistics {
+
+    Map<Long, Integer> durationFrequency = new HashMap<>();
+    File file;
+    StringBuilder builder = new StringBuilder();
 
     public Statistics() {
     }
@@ -11,14 +22,42 @@ public class Statistics {
 
     }
 
-    public void recordData(long duration){
-
-
+    public void updateDurationFrequency(long durationTime){
+       //braucht man diese If oder gehen wir davon einfach aus das man nur Zeiten wählen
+        //kann die es auch nur wirklich gibt
+        // if(durationFrequency.containsKey(durationTime))
+            durationFrequency.put(durationTime, durationFrequency.get(durationTime) + 1);
     }
 
-    public String toString(){
-        return "";
+    public void recordData(int simulatingDay){
+
+        builder.append("Belegungszeit (in Minuten)");
+        builder.append(", ");
+        builder.append("Häufigkeit des Auftretens");
+        builder.append("\r\n");
+        for(Map.Entry<Long, Integer> printMap : durationFrequency.entrySet()){
+            builder.append(printMap.getKey());
+            builder.append(", ");
+            builder.append(printMap.getValue());
+            builder.append("\r\n");
+        }
+        logFile(simulatingDay);
     }
 
+    public void logFile(int simulatingDay){
+        file  = new File("Belegungszeiten_" + simulatingDay + ".txt");
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write(builder.toString().trim());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public long getRandomDuration() {
+        //TODO Implementieren
+        return 1;
+    }
 }
-
